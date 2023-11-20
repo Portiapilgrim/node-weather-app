4693f46cfbdd68f5b2d15c56c5becadff6dd3c04
Juleeya <portiapilgrim@gmail.com>
2023-11-20 17:38:34 +0200

    changed port property

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const weatherData = require("./utils/forecast");
  
 const app = express();
+const port = process.env.PORT || 3000;
 
 //Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, "../public");
@@ -97,6 +98,6 @@ app.get("*", (req,res) => {
         name: "Juleeya"
     });});
 
-app.listen(3000, () => {
-    console.log("Listening on port 3000..."); 
+app.listen(port, () => {
+    console.log("Listening on port" + port); 
 });
